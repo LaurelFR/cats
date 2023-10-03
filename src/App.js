c0c6b494ebd6171d43204791b8889d342b386a5b
LaurelFR <128193405+LaurelFR@users.noreply.github.com>
2023-10-03 15:51:27 -0600

    Added basic project setup and styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import CatFacts from "./CatFacts";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="text-center">Cats</h1>
+        <CatFacts />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/CatFacts.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./CatFacts.css";
+
+export default function CatFacts() {
+  return (
+    <div className="CatFacts d-flex justify-content-center">
+      <button className="btn fact-button">Generate cat facts!</button>
+    </div>
+  );
+}
